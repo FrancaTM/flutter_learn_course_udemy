b4848bc4a640eed7a91a6ff81ec63b85292a72d3
Túlio <tuliofranca@visionnaire.com.br>
2019-01-01 15:59:25 -0200

    Adiciona named routes

--- lib/pages/products.dart
+++ lib/pages/products.dart
@@ -16,12 +16,7 @@ class ProductsPage extends StatelessWidget {
             ),
             ListTile(
               title: Text('Manage Products'),
-              onTap: () => Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                      builder: (BuildContext context) => ProductsAdminPage(),
-                    ),
-                  ),
+              onTap: () => Navigator.pushReplacementNamed(context, '/admin'),
             ),
           ],
         ),
