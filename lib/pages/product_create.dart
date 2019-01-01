78ac4fc372d4aa97436f45a6d461acf512c3940b
Túlio <tuliofranca@visionnaire.com.br>
2019-01-01 15:22:39 -0200

    Adiciona tabs

---
+++ lib/pages/product_create.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ProductCreatePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Create Product'),
+      ),
+      body: Center(),
+    );
+  }
+}
