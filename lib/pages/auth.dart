b9ec2f9dfffe14e073f709d6de035a050c63a908
Túlio <tuliofranca@visionnaire.com.br>
2019-01-05 18:59:11 -0200

    Assignment 4 - User input

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -1,20 +1,56 @@
 import 'package:flutter/material.dart';
 
-import './products.dart';
+class AuthPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _AuthPageState();
+  }
+}
 
-class AuthPage extends StatelessWidget {
+class _AuthPageState extends State<AuthPage> {
+  String _emailAddressValue;
+  String _passwordValue;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Login'),
       ),
-      body: Center(
-        child: RaisedButton(
-          child: Text('Login'),
-          onPressed: () {
-            Navigator.pushReplacementNamed(context, '/');
-          },
+      body: Container(
+        margin: EdgeInsets.all(10.0),
+        child: ListView(
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(labelText: 'Email'),
+              keyboardType: TextInputType.emailAddress,
+              onChanged: (String value) {
+                setState(() {
+                  _emailAddressValue = value;
+                });
+              },
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+              onChanged: (String value) {
+                setState(() {
+                  _passwordValue = value;
+                });
+              },
+            ),
+            SizedBox(height: 10.0),
+            RaisedButton(
+              child: Text('Login'),
+              color: Theme.of(context).accentColor,
+              textColor: Colors.white,
+              onPressed: () {
+                print(_emailAddressValue);
+                print(_passwordValue);
+                Navigator.pushReplacementNamed(context, '/products');
+              },
+            ),
+          ],
         ),
       ),
     );
