68aae991cb1a8e6ef377e652f0836026ab0ca84d
Túlio <tuliofranca@visionnaire.com.br>
2019-01-12 21:14:17 -0200

    Adiciona métodos separados para widgets específicos

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -17,6 +17,64 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
   double _priceValue;
   String _addressValue;
 
+  Widget _buildTitleTextField() {
+    return TextField(
+      decoration: InputDecoration(labelText: 'Product Title'),
+      onChanged: (String value) {
+        setState(() {
+          _titleValue = value;
+        });
+      },
+    );
+  }
+
+  Widget _buildDescriptionTextField() {
+    return TextField(
+      decoration: InputDecoration(labelText: 'Product Description'),
+      maxLines: null,
+      onChanged: (String value) {
+        setState(() {
+          _descriptionValue = value;
+        });
+      },
+    );
+  }
+
+  Widget _buildPriceTextField() {
+    return TextField(
+      decoration: InputDecoration(labelText: 'Product Price'),
+      keyboardType: TextInputType.number,
+      onChanged: (String value) {
+        setState(() {
+          _priceValue = double.parse(value);
+        });
+      },
+    );
+  }
+
+  Widget _buildAddressTextField() {
+    return TextField(
+      decoration: InputDecoration(labelText: 'Product Address'),
+      onChanged: (String value) {
+        setState(() {
+          _addressValue = value;
+        });
+      },
+    );
+  }
+
+  void _submitForm() {
+    final Map<String, dynamic> product = {
+      'title': _titleValue,
+      'description': _descriptionValue,
+      'price': _priceValue,
+      'address': _addressValue,
+      'image': 'assets/food.jpg'
+    };
+    widget.addProduct(product);
+    Navigator.pushReplacementNamed(context, '/products');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -24,57 +82,17 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
       child: ListView(
         children: <Widget>[
           // TODO: Refatorar textfields?
-          TextField(
-            decoration: InputDecoration(labelText: 'Product Title'),
-            onChanged: (String value) {
-              setState(() {
-                _titleValue = value;
-              });
-            },
-          ),
-          TextField(
-            decoration: InputDecoration(labelText: 'Product Description'),
-            maxLines: null,
-            onChanged: (String value) {
-              setState(() {
-                _descriptionValue = value;
-              });
-            },
-          ),
-          TextField(
-            decoration: InputDecoration(labelText: 'Product Price'),
-            keyboardType: TextInputType.number,
-            onChanged: (String value) {
-              setState(() {
-                _priceValue = double.parse(value);
-              });
-            },
-          ),
-          TextField(
-            decoration: InputDecoration(labelText: 'Product Address'),
-            onChanged: (String value) {
-              setState(() {
-                _addressValue = value;
-              });
-            },
-          ),
+          _buildTitleTextField(),
+          _buildDescriptionTextField(),
+          _buildPriceTextField(),
+          _buildAddressTextField(),
           SizedBox(height: 10.0),
           // TODO: Refatorar RaisedButton?
           RaisedButton(
             child: Text('Save'),
             color: Theme.of(context).accentColor,
             textColor: Colors.white,
-            onPressed: () {
-              final Map<String, dynamic> product = {
-                'title': _titleValue,
-                'description': _descriptionValue,
-                'price': _priceValue,
-                'address': _addressValue,
-                'image': 'assets/food.jpg'
-              };
-              widget.addProduct(product);
-              Navigator.pushReplacementNamed(context, '/products');
-            },
+            onPressed: _submitForm,
           ),
         ],
       ),
