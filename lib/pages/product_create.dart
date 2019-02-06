9f72b51743d7063752ae3dcbf2c544e1e54d10e1
Túlio <tuliofranca@visionnaire.com.br>
2019-02-06 19:04:25 -0200

    Adiciona validação ao form

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -21,6 +21,13 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
   Widget _buildTitleTextField() {
     return TextFormField(
       decoration: InputDecoration(labelText: 'Product Title'),
+      // autovalidate: true,
+      validator: (String value) {
+        // if (value.trim().length <= 0) {
+        if (value.isEmpty) {
+          return 'Title is required';
+        }
+      },
       onSaved: (String value) {
         setState(() {
           _titleValue = value;
@@ -65,6 +72,9 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
   }
 
   void _submitForm() {
+    if (!_formKey.currentState.validate()) {
+      return;
+    }
     _formKey.currentState.save();
     final Map<String, dynamic> product = {
       'title': _titleValue,
