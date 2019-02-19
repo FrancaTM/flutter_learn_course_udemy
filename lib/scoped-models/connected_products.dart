2520fb84af36bb9a8e14141471dfa53aef62c957
Túlio <tuliofranca@visionnaire.com.br>
2019-02-19 16:18:53 -0300

    Improve code & fix page transition error

--- lib/scoped-models/connected_products.dart
+++ lib/scoped-models/connected_products.dart
@@ -3,13 +3,13 @@ import 'package:scoped_model/scoped_model.dart';
 import '../models/product.dart';
 import '../models/user.dart';
 
-mixin ConnectedProducts on Model {
-  List<Product> products = [];
-  int selProductIndex;
-  User authenticatedUser;
+mixin ConnectedProductsModel on Model {
+  List<Product> _products = [];
+  int _selProductIndex;
+  User _authenticatedUser;
 
   void unselectProduct() {
-    selProductIndex = null;
+    _selProductIndex = null;
     notifyListeners();
   }
 
@@ -20,11 +20,93 @@ mixin ConnectedProducts on Model {
         description: description,
         image: image,
         price: price,
-        userEmail: authenticatedUser.email,
-        userId: authenticatedUser.id,
+        userEmail: _authenticatedUser.email,
+        userId: _authenticatedUser.id,
         address: address);
-    products.add(newProduct);
-    unselectProduct();
+    _products.add(newProduct);
     notifyListeners();
+  }
+}
+
+mixin ProductsModel on ConnectedProductsModel {
+  bool _showFavorites = false;
+
+  List<Product> get allProducts {
+    return List.from(_products);
+  }
+
+  List<Product> get displayedProducts {
+    if (_showFavorites) {
+      return _products.where((Product product) => product.isFavorite).toList();
+    }
+    return List.from(_products);
+  }
+
+  int get selectedProductIndex {
+    return _selProductIndex;
+  }
+
+  Product get selectedProduct {
+    if (selectedProductIndex == null) {
+      return null;
+    }
+    return _products[selectedProductIndex];
+  }
+
+  bool get displayFavoritesOnly {
+    return _showFavorites;
+  }
+
+  void updateProduct(String title, String description, String image,
+      double price, String address) {
+    final Product updatedProduct = Product(
+        title: title,
+        description: description,
+        image: image,
+        price: price,
+        userEmail: selectedProduct.userEmail,
+        userId: selectedProduct.userId,
+        address: address);
+    _products[selectedProductIndex] = updatedProduct;
+    notifyListeners();
+  }
+
+  void deleteProduct() {
+    _products.removeAt(selectedProductIndex);
+    notifyListeners();
+  }
+
+  void selectProduct(int index) {
+    _selProductIndex = index;
+    notifyListeners();
+  }
+
+  void toggleProductFavoriteStatus() {
+    final bool isCurrentlyFavorite = selectedProduct.isFavorite;
+    final bool newFavoriteState = !isCurrentlyFavorite;
+    // TODO: Refactor and simplify?
+    final Product updatedProduct = Product(
+        title: selectedProduct.title,
+        description: selectedProduct.description,
+        price: selectedProduct.price,
+        image: selectedProduct.image,
+        address: selectedProduct.address,
+        userEmail: selectedProduct.userEmail,
+        userId: selectedProduct.userId,
+        isFavorite: newFavoriteState);
+    _products[selectedProductIndex] = updatedProduct;
+    notifyListeners();
+  }
+
+  void toggleDisplayMode() {
+    _showFavorites = !_showFavorites;
+    notifyListeners();
+  }
+}
+
+mixin UserModel on ConnectedProductsModel {
+  void login(String email, String password) {
+    _authenticatedUser =
+        User(id: 'fasdfasdf', email: email, password: password);
   }
 }
